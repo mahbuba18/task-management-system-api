a444c8825d44c0f2c234d85909bd79d71e373e9d
Mahbuba Islam Priya <mahbubacse18@gmail.com>
2025-02-28 01:29:01 +0600

    updating some error

--- index.js
+++ index.js
@@ -8,11 +8,14 @@ import routes from "./view/routes.js";
 dotenv.config();
 const app = express();
 
+const PORT = process.env.PORT || 8000;
+
+//middleware
 app.use(cors());
 app.use(express.json());
-app.use("/api", routes);
+app.use("/", routes);
 
 connection(); // Connect to the database
 
-const PORT = process.env.PORT || 8000;
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
